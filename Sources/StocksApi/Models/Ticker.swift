2644a307022ae7c67bb99281d1f9901c6a4dcb33
Mok Air <ddoltop@me.com>
2023-10-19 12:00:10 +0900

    add init

--- Sources/StocksApi/Models/Ticker.swift
+++ Sources/StocksApi/Models/Ticker.swift
@@ -20,14 +20,24 @@ public struct Ticker: Codable, Identifiable, Hashable, Equatable {
 //    public let id = UUID()
     
     public let code: String
-    public let name: String
-    public let typeCode: String
-    public let url: String
-    public let reutersCode: String
-    public let nationCode: String
-    public let nationName: String
+    public let name: String?
+    public let typeCode: String?
+    public let url: String?
+    public let reutersCode: String?
+    public let nationCode: String?
+    public let nationName: String?
     
     public var id: String {
         code
+    }
+    
+    public init(code: String, name: String?=nil, typeCode: String?=nil, url: String?=nil, reutersCode: String?=nil, nationCode: String?=nil, nationName: String?=nil) {
+        self.code = code
+        self.name = name
+        self.typeCode = typeCode
+        self.url = url
+        self.reutersCode = reutersCode
+        self.nationCode = nationCode
+        self.nationName = nationName
     }
 }
